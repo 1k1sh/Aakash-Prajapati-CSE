28a903dbafd82b9143ec092f09e6b81b554e0f3c
Aakash <0001ooo1akash@gmail.com>
2022-08-04 11:59:05 +0530

    featuredlist

--- New folder/sortcode/linkedlist/firstLL.py
+++ New folder/sortcode/linkedlist/firstLL.py
@@ -8,15 +8,37 @@ def print_ll(head):
         
         print(ptr.data)
         ptr=ptr.next   
+def insert_inlast(head,data):
+    ptr=head
+    while ptr.next:
+        ptr=ptr.next
+    ptr.next=Node(data)
+    
+def insert_atfirst(head,data):
+    ptr=head
+    head=Node(data)
+    head.next=ptr
     return head
     
+def insert_at_kth(head,data,k):
+    ptr=head
+    count=1
+    while count<k-1:
+        ptr=ptr.next
+        count+=1
+    temp=ptr.next
+    ptr.next=Node(data)
+    ptr.next.next=temp
+    
 
+n=Node(1,Node(2,Node(3,Node(4,Node(5)))))
+insert_inlast(n,9)
+s=insert_atfirst(n,1)
+insert_at_kth(s,10,4)
+print_ll(s)
 
 
   
-
-n=Node(1,Node(2,Node(3,Node(4,Node(5)))))
-print_ll(n)
 
 
 
